f0cb2ba6aa723f85a572a48409edc2177b88e12b
Nandhu9999 <nandhakumar2058@gmail.com>
2023-10-08 02:07:12 +0530

    cleaned comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Hello!
+
+## Project downloaded from Glitch
+
+planning to use this back and forth between glitch and github for easier hosting and coding
+
--- server.js
+++ server.js
@@ -1,42 +1,34 @@
-// Utilities we need
 const fs = require("fs");
 const path = require("path");
+const fastify = require("fastify")({logger: false,});
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+fastify.register(require("@fastify/static"), {root: path.join(__dirname, "public"),prefix: "/",});
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
-
-// Formbody lets us parse incoming forms
 fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
 fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
+  engine: {handlebars: require("handlebars"),},
 });
 
-// Load and parse SEO data
 const seo = require("./src/seo.json");
 if (seo.url === "glitch-default") {
   seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
 }
 
-// We use a module for handling database operations in /src
 const data = require("./src/data.json");
 const db = require("./src/" + data.database);
+
+/*
+ * HOME ROUTE
+*/
 
 fastify.get("/", async (request, reply) => {
   return reply.view("/src/pages/index.hbs", {seo: seo});
 });
+
+
+
+
+
 
 
 // Run the server and report out to the logs
