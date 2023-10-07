4b83dc8578bb67e6773a8b1163398633786dd7af
Nandhu9999 <nandhakumar2058@gmail.com>
2023-10-08 01:38:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+// Utilities we need
+const fs = require("fs");
+const path = require("path");
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// Formbody lets us parse incoming forms
+fastify.register(require("@fastify/formbody"));
+
+// View is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+// Load and parse SEO data
+const seo = require("./src/seo.json");
+if (seo.url === "glitch-default") {
+  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+}
+
+// We use a module for handling database operations in /src
+const data = require("./src/data.json");
+const db = require("./src/" + data.database);
+
+fastify.get("/", async (request, reply) => {
+  return reply.view("/src/pages/index.hbs", {seo: seo});
+});
+
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+  }
+);
