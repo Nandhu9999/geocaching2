61f9af0267fd3ac7e5c0c4c8db00b18dea8d09f7
Nandhu9999 <nandhakumar2058@gmail.com>
2023-10-10 03:01:11 +0530

    cleaned

--- public/js/chatscript.js
+++ public/js/chatscript.js
@@ -2,29 +2,35 @@ console.log("chatscript.js")
 
 function containerSize(){
     const { x, y, width, height } = navigator.virtualKeyboard.boundingRect;
-
     $("#container").style.height = `calc(100% - ${height}px)`;
 }
 
-if ("virtualKeyboard" in navigator) {
-    navigator.virtualKeyboard.overlaysContent = true;
-    navigator.virtualKeyboard.addEventListener("geometrychange", containerSize);
-}
 
 const widgetOpts = $("#widget")
 const textArea = $("#textarea")
 const sendBtn = $("#send")
 
-widgetOpts.addEventListener("click", widgetClicked)
-textArea.addEventListener("click", textareaClicked)
-
-sendBtn.addEventListener("click", sendClicked)
-textArea.addEventListener("keypress", (e)=>{
-    if( e.code == "Enter" && e.shiftKey == false ){
-        sendClicked();
-        e.preventDefault();
+function systemInit(){
+    if ("virtualKeyboard" in navigator) {
+        navigator.virtualKeyboard.overlaysContent = true;
+        navigator.virtualKeyboard.addEventListener("geometrychange", containerSize);
     }
-})
+
+    widgetOpts.addEventListener("click", widgetClicked)
+    textArea.addEventListener("click", textareaClicked)
+    sendBtn.addEventListener("click", sendClicked)
+
+    function KEYDOWN(e){
+        if( e.code == "Enter" && e.shiftKey == false ){
+            sendClicked();
+            e.preventDefault();
+        }
+    }
+
+    if( window.mobileAndTabletCheck ){textArea.addEventListener("keydown", KEYDOWN)}
+    else {textArea.addEventListener("keydown", KEYDOWN)}
+
+}
 
 function widgetClicked(){
     widgetOpts.classList.toggle("active")
@@ -50,11 +56,14 @@ function sendClicked(){
     textArea.focus()
 }
 
-
-
-
 function appendMessage(msg){
     const p = document.createElement("p")
     p.innerText = msg
     $(".chatlogs").append(p)
 }
+
+
+
+
+
+systemInit();
